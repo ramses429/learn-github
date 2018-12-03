3667c87cad3e446a15cb20235c6277796fb3cc47
Ramses <ramses_429@hotmail.com>
2018-12-03 16:39:11 -0500

    new fact 2

--- one.js
+++ one.js
@@ -1,5 +1,19 @@
-class polygon(){
+class polygon_two_sides{
 	
+	constructor( _height , _weigth ) {
+		
+		this.height = _height ; 
+		this.weigth = _weigth ; 
+		
+	}
 	
+	getArea(){
+		
+		return this.height * this.weigth ;
+		
+	}
 	
 }
+
+var square = new polygon_two_sides(2,2);
+console.log( square.getArea() );
